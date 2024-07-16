d5d5b4770990a68002bcc8c75cb217872b89f84b
yocong <rnfaos77@naver.com>
2024-07-16 11:48:15 +0900

    # 15. 이벤트와 유저간의 연관관계 매핑

--- src/main/java/com/study/event/api/event/entity/Event.java
+++ src/main/java/com/study/event/api/event/entity/Event.java
@@ -9,7 +9,7 @@ import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 @Getter
-@ToString
+@ToString(exclude = "eventUser")
 @EqualsAndHashCode(of = "id")
 @NoArgsConstructor
 @AllArgsConstructor
@@ -39,6 +39,9 @@ public class Event {
     @CreationTimestamp
     private LocalDateTime createAt; // 이벤트 등록 날짜
 
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "ev_user_id")
+    private EventUser eventUser;
 
     public void changeEvent(EventSaveDto dto) {
 
